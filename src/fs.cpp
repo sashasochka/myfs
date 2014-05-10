38230e6edaa790651f8ae08e1165a176255709a0
Oleksandr Sochka <sasha.sochka@gmail.com>
2014-05-10 22:08:22 +0300

    Refactorings

--- src/fs.cpp
+++ src/fs.cpp
@@ -4,6 +4,7 @@
 #include <cassert>
 #include <cstring>
 #include <fstream>
+#include <memory>
 
 using namespace std;
 
@@ -34,9 +35,14 @@ auto n_bitmask_blocks = -1;
 auto n_data_blocks = -1;
 fstream fio;
 Link root_link;
+unique_ptr<File> root_dir;
 
 bool is_mounted() {
     return device_capacity != -1 && fio.is_open();
+}
+
+int div_ceil(int a, int b) {
+    return a == 0 ? 0 : (a - 1) / b + 1;
 }
 
 void read_block(int block_id, char* data, int size = BLOCK_SIZE, int shift = 0) {
@@ -133,10 +139,9 @@ int find_empty_block() {
 }
 
 int find_inode_block_id(const string& filename) {
-    File root_dir{root_link.inode_block_id, false};
-    int dir_size = root_dir.size();
-    vector<char> data(static_cast<size_t>(root_dir.size()));
-    root_dir.read(data.data(), dir_size, 0);
+    int dir_size = root_dir->size();
+    vector<char> data(static_cast<size_t>(root_dir->size()));
+    root_dir->read(data.data(), dir_size, 0);
     assert(dir_size % sizeof(Link) == 0);
 
     for (int n_file = 0; n_file < dir_size / sizeof(Link); ++n_file) {
@@ -185,8 +190,8 @@ bool mount(const string& filename) {
     device_capacity = fio.tellg();
 
     // measure how many blocks are used for bitmask
-    n_bitmask_blocks = (device_capacity - 1) / (BLOCK_SIZE * BLOCK_SIZE * 8) + 1;
-    n_data_blocks = (device_capacity - 1) / BLOCK_SIZE + 1 - n_bitmask_blocks;
+    n_bitmask_blocks = div_ceil(device_capacity, BLOCK_SIZE * BLOCK_SIZE * 8);
+    n_data_blocks = div_ceil(device_capacity, BLOCK_SIZE);
     root_link.inode_block_id = n_bitmask_blocks; // use the first block after bitmask
     root_link.filename[0] = '\0';
 
@@ -201,6 +206,7 @@ bool mount(const string& filename) {
         root_inode.type = FileType::Directory;
         write_block(root_link.inode_block_id, &root_inode);
     }
+    root_dir = unique_ptr<File>(new File{root_link.inode_block_id});
 
     return true;
 }
@@ -209,14 +215,14 @@ void umount() {
     device_capacity = -1;
     n_bitmask_blocks = -1;
     n_data_blocks = -1;
+    root_dir.reset();
     fio.close();
 }
 
 string ls() {
-    File root_dir{root_link.inode_block_id, false};
-    int dir_size = root_dir.size();
-    vector<char> data(static_cast<size_t>(root_dir.size()));
-    root_dir.read(data.data(), dir_size, 0);
+    int dir_size = root_dir->size();
+    vector<char> data(static_cast<size_t>(root_dir->size()));
+    root_dir->read(data.data(), dir_size, 0);
     assert(dir_size % sizeof(Link) == 0);
     string result;
     for (int n_file = 0; n_file < dir_size / sizeof(Link); ++n_file) {
@@ -243,15 +249,14 @@ int create(const string & filename, FileType type) {
 
     block_mark_used(inode_block_id);
 
-    File root_dir{root_link.inode_block_id, false};
-    int old_size = root_dir.size();
-    root_dir.truncate(old_size + sizeof(Link));
+    int old_size = root_dir->size();
+    root_dir->truncate(old_size + sizeof(Link));
 
     // Create link in root directory
     Link lnk{};
     lnk.inode_block_id = inode_block_id;
     strcpy(lnk.filename, filename.c_str());
-    root_dir.write(reinterpret_cast<char*>(&lnk), sizeof(Link), old_size);
+    root_dir->write(reinterpret_cast<char*>(&lnk), sizeof(Link), old_size);
 
     INode inode{};
     inode.size = 0;
@@ -268,18 +273,17 @@ bool link(const string& target, const string& name) {
     if (find_inode_block_id(name) != BAD_BLOCK) {
         return false;
     }
-    File root_dir{root_link.inode_block_id, false};
-    int dir_size = root_dir.size();
-    vector<char> data(static_cast<size_t>(root_dir.size()));
-    root_dir.read(data.data(), dir_size, 0);
+    int dir_size = root_dir->size();
+    vector<char> data(static_cast<size_t>(root_dir->size()));
+    root_dir->read(data.data(), dir_size, 0);
     assert(dir_size % sizeof(Link) == 0);
 
     for (int n_file = 0; n_file < dir_size / sizeof(Link); ++n_file) {
         auto& lnk = *reinterpret_cast<Link*>(data.data() + n_file * sizeof(Link));
         if (lnk.filename == target) {
             strcpy(lnk.filename, name.c_str());
-            root_dir.truncate(dir_size + sizeof(Link));
-            root_dir.write(reinterpret_cast<char*>(&lnk), sizeof(Link), dir_size);
+            root_dir->truncate(dir_size + sizeof(Link));
+            root_dir->write(reinterpret_cast<char*>(&lnk), sizeof(Link), dir_size);
 
             // add link
             INode inode;
@@ -296,11 +300,10 @@ bool unlink(const string& filename) {
     if (filename.size() > FILENAME_MAX_LENGTH) {
         return false;
     }
-    File root_dir{root_link.inode_block_id, false};
-    int old_size = root_dir.size();
+    int old_size = root_dir->size();
 
-    vector<char> data(static_cast<size_t>(root_dir.size()));
-    root_dir.read(data.data(), old_size, 0);
+    vector<char> data(static_cast<size_t>(root_dir->size()));
+    root_dir->read(data.data(), old_size, 0);
     assert(old_size % sizeof(Link) == 0);
     for (int n_file = 0; n_file < old_size / sizeof(Link); ++n_file) {
         auto& lnk = *reinterpret_cast<Link*>(data.data() + n_file * sizeof(Link));
@@ -321,9 +324,9 @@ bool unlink(const string& filename) {
                 write_block(lnk.inode_block_id, &inode);
             }
 
-            root_dir.read(reinterpret_cast<char*>(&lnk), sizeof(Link), old_size - sizeof(Link));
-            root_dir.write(reinterpret_cast<char*>(&lnk), sizeof(Link), n_file * sizeof(Link));
-            root_dir.truncate(old_size - sizeof(Link));
+            root_dir->read(reinterpret_cast<char*>(&lnk), sizeof(Link), old_size - sizeof(Link));
+            root_dir->write(reinterpret_cast<char*>(&lnk), sizeof(Link), n_file * sizeof(Link));
+            root_dir->truncate(old_size - sizeof(Link));
             return true;
         }
     }
@@ -365,7 +368,7 @@ string File::filestat() const {
     result += "Blocks uses(";
     string blocks;
     int blocks_used = 0;
-    for (int i = 0; i < (inode.size == 0 ? 0 : (inode.size - 1) / BLOCK_SIZE + 1); ++i) {
+    for (int i = 0; i < div_ceil(inode.size, BLOCK_SIZE); ++i) {
         if (inode.data_block_ids[i] >= 0) {
             blocks += '#';
             blocks += to_string(inode.data_block_ids[i]);
@@ -477,8 +480,8 @@ bool File::truncate(int size) {
 
     if (size == inode.size) return true;
 
-    int n_old_blocks = inode.size == 0 ? 0 : (inode.size - 1) / BLOCK_SIZE + 1;
-    int n_blocks = size == 0 ? 0 : (size - 1) / BLOCK_SIZE + 1;
+    int n_old_blocks = div_ceil(inode.size, BLOCK_SIZE);
+    int n_blocks = div_ceil(size, BLOCK_SIZE);
     assert(n_blocks <= BLOCKS_PER_INODE && "too big file");
     if (n_blocks < n_old_blocks) {
         for (int block_index = n_blocks; block_index < n_old_blocks; ++block_index) {
